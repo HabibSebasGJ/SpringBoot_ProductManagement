1ee663f994662a6afc6b2eba4872badcf02f23cc
Habib Gonzalez <habibsebastiangj@gmail.com>
2025-01-21 12:19:45 -0500

    Craeted lessPrice, search and create in controller

--- src/main/java/com/products/Products/Controller/ProductController.java
+++ src/main/java/com/products/Products/Controller/ProductController.java
@@ -1,5 +1,41 @@
 package com.products.Products.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.products.Products.Model.Product;
+import com.products.Products.Repository.ProductRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping("/create")
+    public Product createProduct(@RequestBody Product product){
+        return productRepository.save(product);
+    } 
+
+    @GetMapping("/lessPrice/{price}")
+    public List<Product> productLess(@PathVariable double price){
+        return productRepository.findByPriceLessThan(price);
+    } 
+    
+    @GetMapping("/search")
+    public List<Product> searchProduct(@RequestParam String name){
+        return productRepository.findByNameContaining(name);
+    }
     
 }
