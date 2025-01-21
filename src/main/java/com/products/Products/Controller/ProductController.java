3dc05afb7359c7d99edc075fa391a17e9e1f91b4
Habib Gonzalez <habibsebastiangj@gmail.com>
2025-01-20 19:17:16 -0500

    Created model, reposiroty and controller(not finished)

---
+++ src/main/java/com/products/Products/Controller/ProductController.java
@@ -0,0 +1,5 @@
+package com.products.Products.Controller;
+
+public class ProductController {
+    
+}
---
+++ src/main/java/com/products/Products/Repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.products.Products.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.products.Products.Model.Product;
+
+public interface ProductRepository extends JpaRepository <Product, Long> {
+    List<Product>findByPriceLessThan(double price);
+    List<Product>findByNameContaining(String name);
+    
+}
